e5b36889e5ffb14363ca67099bd37ca6e6036c45
ParvPatel01 <patel.parv1@northeastern.edu>
2024-06-14 01:51:22 +0530

    Update Database After every 1min feature added

--- Bitcoin-Explorer-Backend/src/main.rs
+++ Bitcoin-Explorer-Backend/src/main.rs
@@ -1,37 +1,41 @@
-use reqwest::blocking::Client;
 use serde::Deserialize;
-use serde_json::json;
-use sqlx::{pool, query, Row};
-use core::{hash, time};
-use std::{error::Error, result};
+use std::{error::Error, thread::sleep, time::{Duration, Instant}};
 
 #[derive(Debug, Deserialize)]
 struct LatestBlock {
     hash: String,
     time: u64,
     block_index: i64,
-    height: i64
+    height: i64,
 }
 
-async fn create_latest_block(latestBlock: &LatestBlock, pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
-    let query = "INSERT INTO latest_block (hash, time, block_index, height) VALUES ($1, $2, $3, $4)";
+async fn create_latest_block(
+    latest_block: &LatestBlock,
+    pool: &sqlx::PgPool,
+) -> Result<(), sqlx::Error> {
+    let query =
+        "INSERT INTO latest_block (hash, time, block_index, height) VALUES ($1, $2, $3, $4)";
     sqlx::query(query)
-        .bind(&latestBlock.hash)
-        .bind(&(latestBlock.time as i64))
-        .bind(&(latestBlock.block_index as i64))
-        .bind(&(latestBlock.height as i64))
+        .bind(&latest_block.hash)
+        .bind(&(latest_block.time as i64))
+        .bind(&(latest_block.block_index as i64))
+        .bind(&(latest_block.height as i64))
         .execute(pool)
         .await?;
-
+    println!("Inserted Latest Block: {:?}", latest_block);
     Ok(())
 }
 
-async fn update_latest_block(latestBlock: &LatestBlock, hash: &String, pool: &sqlx::PgPool) -> Result<(), sqlx::Error> {
+async fn update_latest_block(
+    latest_block: &LatestBlock,
+    hash: &String,
+    pool: &sqlx::PgPool,
+) -> Result<(), sqlx::Error> {
     let query = "UPDATE latest_block SET time = $1, block_index = $2, height = $3 WHERE hash = $4";
     sqlx::query(query)
-        .bind(&(latestBlock.time as i64))
-        .bind(&(latestBlock.block_index as i64))
-        .bind(&(latestBlock.height as i64))
+        .bind(&(latest_block.time as i64))
+        .bind(&(latest_block.block_index as i64))
+        .bind(&(latest_block.height as i64))
         .bind(&hash)
         .execute(pool)
         .await?;
@@ -54,23 +58,32 @@ async fn update_latest_block(latestBlock: &LatestBlock, hash: &String, pool: &sq
 //     Ok(())
 // }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
-    let url = "postgres://myadmin:Qwert12345@bitcoin.postgres.database.azure.com:5432/postgres";
-    let pool = sqlx::postgres::PgPool::connect(url).await?;
-
-    sqlx::migrate!("./migrations").run(&pool).await?;
-
-    let latest_block = blocking_get().await?;
-
-    create_latest_block(&latest_block, &pool).await?;
-
-    Ok(())
-}
-
-async fn blocking_get() -> Result<(LatestBlock), Box<dyn Error>> {
+async fn blocking_get() -> Result<LatestBlock, Box<dyn Error>> {
     let res = reqwest::get("https://blockchain.info/latestblock").await?;
     let latest_block: LatestBlock = res.json().await?;
+    println!("Fetched Latest Block: {:?}", latest_block);
+    Ok(latest_block)
+}
 
-    Ok((latest_block))
+async fn upload_latest_block_every_n_minutes(t: u64, pool: sqlx::PgPool) {
+    let interval = Duration::from_secs(t * 60);
+    let mut next_time = Instant::now() + interval;
+    loop {
+        let latest_block = blocking_get().await.unwrap();
+        create_latest_block(&latest_block, &pool).await.unwrap();
+        println!("Latest Block Added! (10min interval started)");
+        sleep(next_time - Instant::now());
+        next_time += interval;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let url = "postgres://myadmin:Qwert12345@bitcoin.postgres.database.azure.com:5432/postgres";
+    let pool = sqlx::postgres::PgPool::connect(url).await.unwrap();
+
+    sqlx::migrate!("./migrations").run(&pool).await;
+
+    
+    upload_latest_block_every_n_minutes(1, pool).await;
 }
